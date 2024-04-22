a11fc12694f2cc155e5d05125dc720cdcbb6306c
AzureAD\JeromeChallet <cjerome@premiumartsinc.com>
2024-04-22 11:41:42 +0900

    Selecting Creating Deleting Elements

---
+++ README.md
@@ -0,0 +1,14 @@
+document.documentElement\
+document.head\
+document.body\
+document.querySelector('.className');\
+document.querySelectorAll('.className');\
+document.getElementById('idName');\
+document.getElementsByClassName('elementName');\
+document.createElement('div');\
+var.innerHTML = ...\
+element.append(varName)\
+element.prepend(varName)\
+element.before(varName)\
+element.after(varName)\
+message.parentElement.removeChild(varName);\
--- script.js
+++ script.js
@@ -29,3 +29,52 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////SELECTING CREATING AND DELETING ELEMENTS/////////////////////
+// selecting elements
+//selecting the document element
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+//select multiple elements
+document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+//all the elements with name of buutton
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+// .insertAdjacentHTML
+
+//add a cookie message at the bottom of the screen
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionalities';
+message.innerHTML =
+  'We use cookies for improved functionalities. <button class="btn btn--close-cookie">Got it!</button>';
+
+// one element can only be at one place at a time so it can be both at the begining and end
+// prepdending add as the 1st child of the element
+//header.prepend(message);
+//appending add as the last child of the element
+header.append(message);
+//insert multiple copies of the same element by cloning
+//header.append(message.cloneNode(true));
+
+//header.before(message);
+//header.after(message);
+
+// delete elements
+// moving up and down the DOM tree is called DOM traversing
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    //message.remove();
+    message.parentElement.removeChild(message);
+  });
