bc12c5accabe10864f5be6256f9b2e8a15c390c4
AzureAD\JeromeChallet <cjerome@premiumartsinc.com>
2024-04-23 14:26:15 +0900

    Lazy Loading Images

--- script.js
+++ script.js
@@ -126,6 +126,30 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+//////////////////LAZY LOADING IMAGES/////////////////////
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 //////////////////EVENT DELEGATION/////////////////////
 // page navigation
 
